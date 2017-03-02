738be6d54b7a627fdaba21e21ba440bf19c220b3
IOS <ios@SC-01s-Mac-mini.local>
2017-03-02 09:41:47 +0700

    First Commit

--- meong/ViewController.swift
+++ meong/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var m_name: UILabel!
+    
+    @IBOutlet weak var login_button: UIButton!
+    
+    @IBOutlet weak var m_username: UITextField!
+    
+    @IBOutlet weak var m_password: UITextField!
+    
+    @IBOutlet weak var m_text_view: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func button_kepencet(_ sender: Any) {
+        if(m_username.text?.characters.count == 0){
+            let alertVC = UIAlertController(title: "Alert", message: "All Field Must Have Value", preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "OK", style: .default, handler: { (alertAction) in
+                print("OK Kepencet")
+            })
+            alertVC.addAction(okButton)
+            self.show(alertVC, sender: nil)
+            return
+        }
+        let alertVC = UIAlertController(title: "You Are", message: "Username : \(m_username.text!) and Password : \(m_password.text!), Saved", preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default, handler: { (alertAction) in
+            print("OK Kepencet")
+        })
+            
+        alertVC.addAction(okButton)
+        self.show(alertVC, sender: nil)
+        m_text_view.text = "\(m_text_view.text!)  \(m_username.text!)"
+    }
 
 }
 
